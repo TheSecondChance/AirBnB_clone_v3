e8b91223fce787051e577a7d82b181a77a19955b
TheSecondChance <mesakmegerem@gmail.com>
2024-02-02 09:55:21 -0800

    fix someIsuue

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,6 +69,19 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+        or None if not found
+        """
+        if cls not in classes.values():
+            return(None)
+        else:
+            huluKefl = models.storage.all(cls)
+            for value in huluKefl.values():
+                if (value.id == id):
+                    return value
+            return(None)
+
     def count(self, cls=None):
         """Returns the number of objects in storage matching the given class.
         If no class is passed, returns the count of all objects in storage
