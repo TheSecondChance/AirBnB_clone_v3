15d888fdbccedb458cf8719e63c81752627a9e79
TheSecondChance <mesakmegerem@gmail.com>
2024-02-03 01:54:57 -0800

    fix error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
